7651ac14f74c9ff51b16f8c450b2ba2c1fe809c8
Will Albeck <will@albeck.me>
2025-11-03 21:34:12 -0500

    feat: Fetch and display breed list on home page

--- src/components/BreedCard.js
+++ src/components/BreedCard.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link for navigation
 
-// We will eventually pass 'breed' as a prop
-function BreedCard() { 
+// We are now receiving the 'breed' object as a prop from BreedList
+function BreedCard({ breed }) {
+  
+  // A safety check: some breeds in the API don't have an image.
+  // If breed.image exists, use its url. If not, use a placeholder.
+  const imageUrl = breed.image ? breed.image.url : "https://via.placeholder.com/300x200";
+
   return (
-    <div style={{ border: "1px solid #ccc", padding: "1rem", margin: "0.5rem" }}>
-      <h4>Breed Name Placeholder</h4>
-      {/* We'll add the Link wrapper and image here */}
-    </div>
+    // Wrap the card in a Link that goes to our dynamic route
+    <Link to={`/breed/${breed.id}`} style={{ textDecoration: 'none', color: 'black' }}>
+      <div style={{
+        border: "1px solid #ccc",
+        borderRadius: "8px",
+        padding: "1rem",
+        margin: "1rem",
+        width: "300px",
+        textAlign: "center",
+        boxShadow: "0 2px 4px rgba(0,0,0,0.1)"
+      }}>
+        <img 
+          src={imageUrl} 
+          alt={breed.name} 
+          style={{ width: "100%", height: "200px", objectFit: "cover", borderRadius: "4px" }} 
+        />
+        <h3 style={{ marginTop: "1rem" }}>{breed.name}</h3>
+      </div>
+    </Link>
   );
 }
 
