fa2a80fb28934f290e1f49f08eabf9fe0b9ef512
Will Albeck <will@albeck.me>
2025-11-03 21:20:19 -0500

    feat: Create component skeletons and wire up App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// We'll create these components in the next steps.
-// For now, we'll just use placeholder functions.
-const BreedList = () => <h2>Home Page: Breed List</h2>;
-const BreedDetail = () => <h2>Details Page for One Breed</h2>;
-
-// Placeholder NavBar
-const NavBar = () => (
-  <nav style={{ padding: '1rem', backgroundColor: '#eee' }}>
-    <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
-  </nav>
-);
+// Import our real components
+import NavBar from './components/NavBar';
+import BreedList from './components/BreedList';
+import BreedDetail from './components/BreedDetail';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        <Route path="/breed/:id" element={<BreedDetail />} />
-        <Route path="/" element={<BreedList />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/breed/:id" element={<BreedDetail />} />
+          <Route path="/" element={<BreedList />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/components/BreedList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BreedList() {
+  return (
+    <div>
+      <h2>Dog Breed Finder</h2>
+      {/* We will add the 3-state (loading, error, data) fetch logic here */}
+    </div>
+  );
+}
+
+export default BreedList;
